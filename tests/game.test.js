2ff1ac1d935c5f4ce55c3cfa94e86bad9e60c559
Leo Melin <leo.melin@gofore.com>
2017-10-06 17:14:54 +0300

    Two bot game winner test

--- tests/game.test.js
+++ tests/game.test.js
@@ -269,6 +269,77 @@ describe('test game logic', () => {
     expect(eventsList[6].event).toBe('GAME_ENDED');
     expect(eventsList[6].data.result).toBe('TIE');
   });
+
+  test('should make bot named Paul win', async () => {
+    const eventsList = [];
+    const socket = (eventInfo) => {
+      eventsList.push(eventInfo);
+    };
+    const game = new Game({
+      'setup': {
+        'edgeLength': 8,
+        'speed': 0,
+        'numOfTasksPerTick': 1,
+        'playerStartPositions': [
+          {
+            'name': 'John',
+            'x': 0,
+            'y': 0,
+            'z': 0
+          },
+          {
+            'name': 'Paul',
+            'x': 0,
+            'y': 0,
+            'z': 1
+          }
+        ]
+      },
+      'players': [
+        {
+          'name': 'John',
+          'url': (nextTickInfo) => {
+            return [{
+              task: 'MOVE',
+              direction: '+Y'
+            }];
+          }
+        },
+        {
+          'name': 'Paul',
+          'url': (nextTickInfo) => {
+            return [{
+              task: 'BOMB',
+              x: 0,
+              y: 1,
+              z: 0
+            }];
+          }
+        }
+      ]
+    }, createMockSocket(socket));
+
+    game.start();
+
+    // Wait game to finish
+    await wait(1000);
+    expect(eventsList.length).toBe(6);
+    expect(eventsList[0].event).toBe('GAME_STARTED');
+    expect(eventsList[1].event).toBe('NEXT_TICK');
+    expect(eventsList[2].event).toBe('PLAYER_MOVE_ATTEMPT');
+    expect(eventsList[3].event).toBe('PLAYER_PLACED_BOMB');
+    expect(eventsList[4].event).toBe('PLAYER_LOST');
+    expect(eventsList[4].data).toMatchObject({
+      name: 'John',
+      cause: 'Player stepped on a BOMB'
+    });
+    expect(eventsList[5].event).toBe('GAME_ENDED');
+    expect(eventsList[5].data.result).toBe('WINNER_FOUND');
+    expect(eventsList[5].data.winner).toMatchObject({
+      name: 'Paul',
+      highScore: 1
+    });
+  });
 });
 
 
