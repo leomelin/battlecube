261a4a59ed64e8b7fcecae27eca42fcf02ceccee
Leo Melin <leo.melin@gofore.com>
2017-10-06 17:04:51 +0300

    Player collision test

--- tests/game.test.js
+++ tests/game.test.js
@@ -91,6 +91,7 @@ describe('test game logic', () => {
     expect(eventsList[1].data.players[0].y).toBe(0);
     expect(eventsList[1].data.players[0].z).toBe(0);
 
+    expect(eventsList[4].data.result).toBe('TIE');
   });
 
   test('should run game and make bot lose by stepping on a bomb', async () => {
@@ -141,6 +142,7 @@ describe('test game logic', () => {
       cause: 'Player stepped on a BOMB'
     });
     expect(eventsList[4].event).toBe('GAME_ENDED');
+    expect(eventsList[4].data.result).toBe('TIE');
   });
 
   test('should make bot lose on giving too many directions', async () => {
@@ -193,6 +195,79 @@ describe('test game logic', () => {
       cause: { 'details': 'Invalid amount of directions sent', 'error': 'VALIDATION_ERROR' }
     });
     expect(eventsList[3].event).toBe('GAME_ENDED');
+    expect(eventsList[3].data.result).toBe('TIE');
+  });
+
+  test('should make two bots collide, both should lose', async () => {
+    const eventsList = [];
+    const socket = (eventInfo) => {
+      eventsList.push(eventInfo);
+    };
+    const game = new Game({
+      'setup': {
+        'edgeLength': 8,
+        'speed': 0,
+        'numOfTasksPerTick': 1,
+        'playerStartPositions': [
+          {
+            'name': 'John',
+            'x': 0,
+            'y': 0,
+            'z': 0
+          },
+          {
+            'name': 'Paul',
+            'x': 0,
+            'y': 0,
+            'z': 1
+          }
+        ]
+      },
+      'players': [
+        {
+          'name': 'John',
+          'url': (nextTickInfo) => {
+            return [{
+              task: 'MOVE',
+              direction: '+Z'
+            }];
+          }
+        },
+        {
+          'name': 'Paul',
+          'url': (nextTickInfo) => {
+            return [{
+              task: 'BOMB',
+              x: 1,
+              y: 2,
+              z: 3
+            }];
+          }
+        }
+      ]
+    }, createMockSocket(socket));
+
+    game.start();
+
+    // Wait game to finish
+    await wait(1000);
+    expect(eventsList.length).toBe(7);
+    expect(eventsList[0].event).toBe('GAME_STARTED');
+    expect(eventsList[1].event).toBe('NEXT_TICK');
+    expect(eventsList[2].event).toBe('PLAYER_MOVE_ATTEMPT');
+    expect(eventsList[3].event).toBe('PLAYER_PLACED_BOMB');
+    expect(eventsList[4].event).toBe('PLAYER_LOST');
+    expect(eventsList[4].data).toMatchObject({
+      name: 'John',
+      cause: 'Player crashed to other player'
+    });
+    expect(eventsList[5].event).toBe('PLAYER_LOST');
+    expect(eventsList[5].data).toMatchObject({
+      name: 'Paul',
+      cause: 'Player crashed to other player'
+    });
+    expect(eventsList[6].event).toBe('GAME_ENDED');
+    expect(eventsList[6].data.result).toBe('TIE');
   });
 });
 
